60081e685abbe905a4bbed9f35bcd94d78d3ff7c
mfranca915 <mfranca915@gmail.com>
2021-10-18 20:15:50 -0400

    Initial Project Commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.velocity = transform.forward * speed;
+    }
+}
